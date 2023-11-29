ee1c41ada0e1baa5ce92f7362ecb24609e09c625
alaynadelauro <andelauro@yahoo.com>
2023-11-29 16:42:23 -0700

    init now that I think I moved everything right

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'codedragondomain.us.auth0.com'
+export const clientId = 'CRC615W0cmv52NbdhsaOQ7ob0Z6KwtiL'
+export const audience = 'https://firebreathinglizard.com'
