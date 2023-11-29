3cb4c9cf914f3930a9779d45c0373cbeab3bd6d5
alaynadelauro <andelauro@yahoo.com>
2023-11-29 16:50:23 -0700

    got everything working again

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:7045' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'codedragondomain.us.auth0.com'
 export const clientId = 'CRC615W0cmv52NbdhsaOQ7ob0Z6KwtiL'
