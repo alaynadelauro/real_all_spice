b375299e0ad785c0e858d3fa2a76a1162f71ffa9
alaynadelauro <andelauro@yahoo.com>
2023-12-06 14:05:18 -0700

    putting favorites on hold, doing ingredients

--- client/src/services/AccountService.js
+++ client/src/services/AccountService.js
@@ -1,13 +1,19 @@
 import { AppState } from '../AppState'
 import { Account } from '../models/Account.js'
+import { Favorite } from '../models/Favorite'
 import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
+import { favoritesService } from './FavoritesService'
 
 class AccountService {
   async getAccount() {
     try {
       const res = await api.get('/account')
       AppState.account = new Account(res.data)
+      // debugger
+      // const favorites = await favoritesService.getFavorites()
+      // logger.log(favorites)
+      // AppState.favorites = favorites.data.map((favorite) => new Favorite(favorite))
     } catch (err) {
       logger.error('HAVE YOU STARTED YOUR SERVER YET???', err)
     }
